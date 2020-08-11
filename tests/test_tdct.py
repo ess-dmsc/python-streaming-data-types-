387f88b5ec7d71752b6c2841a748bd721acbc326
Matthew D Jones <matthew.d.jones@tessella.com>
2020-08-11 09:18:41 +0100

    Add test case for single timestamp

--- tests/test_tdct.py
+++ tests/test_tdct.py
@@ -27,13 +27,20 @@ class TestSerialisationTdct:
     def test_serialises_and_deserialises_tdct_message_with_array_of_timestamps(self):
         original_entry = {"name": "some_name", "timestamps": np.array([0, 1, 2, 3, 4])}
 
-        buf = serialise_tdct(**self.original_entry)
+        buf = serialise_tdct(**original_entry)
         deserialised_tuple = deserialise_tdct(buf)
 
         assert deserialised_tuple.name == original_entry["name"]
-        assert np.allclose(
-            deserialised_tuple.timestamps, self.original_entry["timestamps"]
-        )
+        assert np.allclose(deserialised_tuple.timestamps, original_entry["timestamps"])
+
+    def test_serialises_and_deserialises_tdct_message_with_single_timestamp(self):
+        original_entry = {"name": "some_name", "timestamps": np.array(0)}
+
+        buf = serialise_tdct(**original_entry)
+        deserialised_tuple = deserialise_tdct(buf)
+
+        assert deserialised_tuple.name == original_entry["name"]
+        assert np.allclose(deserialised_tuple.timestamps, original_entry["timestamps"])
 
     def test_if_buffer_has_wrong_id_then_throws(self):
         buf = serialise_tdct(**self.original_entry)
