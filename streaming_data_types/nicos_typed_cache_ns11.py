bf128af366fce8645f322c49343f7de566f7f3e3
AÜC Hardal <umit.hardal@ess.eu>
2020-09-21 11:23:38 +0200

    Add serialisers for main types

--- streaming_data_types/nicos_typed_cache_ns11.py
+++ streaming_data_types/nicos_typed_cache_ns11.py
@@ -1,10 +1,69 @@
 from collections import namedtuple
 import flatbuffers
+import numpy as np
 from streaming_data_types.fbschemas.nicos_typed_cache_ns11 import TypedCacheEntry
+from streaming_data_types.fbschemas.nicos_typed_cache_ns11.Bool import (
+    BoolStart,
+    BoolAddValue,
+    BoolEnd,
+)
+from streaming_data_types.fbschemas.nicos_typed_cache_ns11.Double import (
+    DoubleStart,
+    DoubleAddValue,
+    DoubleEnd,
+)
+from streaming_data_types.fbschemas.nicos_typed_cache_ns11.Long import (
+    LongStart,
+    LongAddValue,
+    LongEnd,
+)
+from streaming_data_types.fbschemas.nicos_typed_cache_ns11.String import (
+    StringStart,
+    StringAddValue,
+    StringEnd,
+)
+from streaming_data_types.fbschemas.nicos_typed_cache_ns11.Value import Value
 from streaming_data_types.utils import check_schema_identifier
 
 
 FILE_IDENTIFIER = b"ns11"
+
+
+def _serialise_long(builder: flatbuffers.Builder, data: np.ndarray):
+    LongStart(builder)
+    LongAddValue(builder, data.item())
+    value_position = LongEnd(builder)
+    TypedCacheEntry.TypedCacheEntryStart(builder)
+    TypedCacheEntry.TypedCacheEntryAddValue(builder, value_position)
+    TypedCacheEntry.TypedCacheEntryAddValueType(builder, Value.Long)
+
+
+def _serialise_string(builder: flatbuffers.Builder, data: np.ndarray):
+    string_offset = builder.CreateString(data.item())
+    StringStart(builder)
+    StringAddValue(builder, string_offset)
+    value_position = StringEnd(builder)
+    TypedCacheEntry.TypedCacheEntryStart(builder)
+    TypedCacheEntry.TypedCacheEntryAddValue(builder, value_position)
+    TypedCacheEntry.TypedCacheEntryAddValueType(builder, Value.String)
+
+
+def _serialise_double(builder: flatbuffers.Builder, data: np.ndarray):
+    DoubleStart(builder)
+    DoubleAddValue(builder, data.item())
+    value_position = DoubleEnd(builder)
+    TypedCacheEntry.TypedCacheEntryStart(builder)
+    TypedCacheEntry.TypedCacheEntryAddValue(builder, value_position)
+    TypedCacheEntry.TypedCacheEntryAddValueType(builder, Value.Double)
+
+
+def _serialise_bool(builder: flatbuffers.Builder, data: np.ndarray):
+    BoolStart(builder)
+    BoolAddValue(builder, data.item())
+    value_position = BoolEnd(builder)
+    TypedCacheEntry.TypedCacheEntryStart(builder)
+    TypedCacheEntry.TypedCacheEntryAddValue(builder, value_position)
+    TypedCacheEntry.TypedCacheEntryAddValueType(builder, Value.Bool)
 
 
 def serialise_ns11(
