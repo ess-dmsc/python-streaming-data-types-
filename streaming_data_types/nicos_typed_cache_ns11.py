3be512847cb6bc108f29a05479dc76bcbd7f33e2
AÜC Hardal <umit.hardal@ess.eu>
2020-09-21 13:33:42 +0200

    Add main serialiser for non-array types

--- streaming_data_types/nicos_typed_cache_ns11.py
+++ streaming_data_types/nicos_typed_cache_ns11.py
@@ -1,4 +1,6 @@
 from collections import namedtuple
+from typing import Any, Callable, Dict
+
 import flatbuffers
 import numpy as np
 from streaming_data_types.fbschemas.nicos_typed_cache_ns11 import TypedCacheEntry
@@ -27,6 +29,25 @@ from streaming_data_types.utils import check_schema_identifier
 
 
 FILE_IDENTIFIER = b"ns11"
+
+
+def _complete_buffer(
+    builder,
+    key: str,
+    time_stamp: float = 0,
+    ttl: float = 0,
+    expired: bool = False,
+):
+    TypedCacheEntry.TypedCacheEntryAddExpired(builder, expired)
+    TypedCacheEntry.TypedCacheEntryAddTtl(builder, ttl)
+    TypedCacheEntry.TypedCacheEntryAddTime(builder, time_stamp)
+    TypedCacheEntry.TypedCacheEntryAddKey(builder, key)
+    cache_entry_message = TypedCacheEntry.TypedCacheEntryEnd(builder)
+    builder.Finish(cache_entry_message)
+
+    buffer = builder.Output()
+    buffer[4:8] = FILE_IDENTIFIER
+    return buffer
 
 
 def _serialise_long(builder: flatbuffers.Builder, data: np.ndarray):
@@ -66,36 +87,54 @@ def _serialise_bool(builder: flatbuffers.Builder, data: np.ndarray):
     TypedCacheEntry.TypedCacheEntryAddValueType(builder, Value.Bool)
 
 
+_map_scalar_type_to_serialiser = {
+    np.dtype("int32"): _serialise_long,
+    np.dtype("int64"): _serialise_long,
+    np.dtype("float32"): _serialise_double,
+    np.dtype("float64"): _serialise_double,
+    np.dtype("bool"): _serialise_bool,
+}
+
+
 def serialise_ns11(
+    value: Any,
     key: str,
-    valueType: int,
-    value: str,
     time_stamp: float = 0,
     ttl: float = 0,
     expired: bool = False,
 ):
     builder = flatbuffers.Builder(128)
-
-    value_offset = builder.CreateString(value)
     key_offset = builder.CreateString(key)
+    value = np.array(value)
 
-    TypedCacheEntry.TypedCacheEntryStart(builder)
-    TypedCacheEntry.TypedCacheEntryAddValue(builder, value_offset)
-    TypedCacheEntry.TypedCacheEntryAddValueType(builder, valueType)
-    TypedCacheEntry.TypedCacheEntryAddExpired(builder, expired)
-    TypedCacheEntry.TypedCacheEntryAddTtl(builder, ttl)
-    TypedCacheEntry.TypedCacheEntryAddTime(builder, time_stamp)
-    TypedCacheEntry.TypedCacheEntryAddKey(builder, key_offset)
-    cache_entry_message = TypedCacheEntry.TypedCacheEntryEnd(builder)
-    builder.Finish(cache_entry_message)
-
-    # Generate the output and replace the file_identifier
-    buffer = builder.Output()
-    buffer[4:8] = FILE_IDENTIFIER
-
-    return bytes(buffer)
+    if value.ndim == 0:
+        _serialise_value(
+            builder, value, _serialise_string, _map_scalar_type_to_serialiser
+        )
+    return bytes(_complete_buffer(builder, key_offset, time_stamp, ttl, expired))
 
 
+def _serialise_value(
+    builder: flatbuffers.Builder,
+    value: Any,
+    string_serialiser: Callable,
+    serialisers_map: Dict,
+):
+    if np.issubdtype(value.dtype, np.unicode_) or np.issubdtype(
+        value.dtype, np.string_
+    ):
+        string_serialiser(builder, value)
+    else:
+        try:
+            serialisers_map[value.dtype](builder, value)
+        except KeyError:
+            raise NotImplementedError(
+                f"Cannot serialise data of type {value.dtype}, must use one of "
+                f"{list(_map_scalar_type_to_serialiser.keys()).append(np.unicode_)}"
+            )
+
+
+# TODO: Following function should be rewritten.
 def deserialise_ns11(buffer):
     check_schema_identifier(buffer, FILE_IDENTIFIER)
 
