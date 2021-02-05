fbd7e6bc7cf3aed41e9bcaadfddfa7ed38f7af47
Jonas Nilsson <mjc.nilsson@gmail.com>
2021-02-05 15:34:27 +0100

    Version bump.

--- streaming_data_types/__init__.py
+++ streaming_data_types/__init__.py
@@ -18,7 +18,7 @@ from streaming_data_types.forwarder_config_update_rf5k import (
 )
 from streaming_data_types.area_detector_NDAr import deserialise_ndar, serialise_ndar
 
-__version__ = "0.9.6"
+__version__ = "0.10.0"
 
 SERIALISERS = {
     "ev42": serialise_ev42,
