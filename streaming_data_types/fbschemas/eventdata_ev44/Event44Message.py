5d0a08ccbbb4b7dfb432adf218251d97750ad487
Kenan Muric <kenan.muric@ess.eu>
2022-11-11 14:30:51 +0100

    patching ev44 schema

--- streaming_data_types/fbschemas/eventdata_ev44/Event44Message.py
+++ streaming_data_types/fbschemas/eventdata_ev44/Event44Message.py
@@ -6,7 +6,6 @@ import flatbuffers
 from flatbuffers.compat import import_numpy
 np = import_numpy()
 
-
 class Event44Message(object):
     __slots__ = ['_tab']
 
@@ -17,6 +16,10 @@ class Event44Message(object):
         x.Init(buf, n + offset)
         return x
 
+    @classmethod
+    def GetRootAsEvent44Message(cls, buf, offset=0):
+        """This method is deprecated. Please switch to GetRootAs."""
+        return cls.GetRootAs(buf, offset)
     @classmethod
     def Event44MessageBufferHasIdentifier(cls, buf, offset, size_prefixed=False):
         return flatbuffers.util.BufferHasIdentifier(buf, offset, b"\x65\x76\x34\x34", size_prefixed=size_prefixed)
@@ -33,8 +36,15 @@ class Event44Message(object):
         return None
 
     # Event44Message
-    def ReferenceTime(self, j):
+    def MessageId(self):
         o = flatbuffers.number_types.UOffsetTFlags.py_type(self._tab.Offset(6))
+        if o != 0:
+            return self._tab.Get(flatbuffers.number_types.Int64Flags, o + self._tab.Pos)
+        return 0
+
+    # Event44Message
+    def ReferenceTime(self, j):
+        o = flatbuffers.number_types.UOffsetTFlags.py_type(self._tab.Offset(8))
         if o != 0:
             a = self._tab.Vector(o)
             return self._tab.Get(flatbuffers.number_types.Int64Flags, a + flatbuffers.number_types.UOffsetTFlags.py_type(j * 8))
@@ -42,26 +52,26 @@ class Event44Message(object):
 
     # Event44Message
     def ReferenceTimeAsNumpy(self):
-        o = flatbuffers.number_types.UOffsetTFlags.py_type(self._tab.Offset(6))
+        o = flatbuffers.number_types.UOffsetTFlags.py_type(self._tab.Offset(8))
         if o != 0:
             return self._tab.GetVectorAsNumpy(flatbuffers.number_types.Int64Flags, o)
         return 0
 
     # Event44Message
     def ReferenceTimeLength(self):
-        o = flatbuffers.number_types.UOffsetTFlags.py_type(self._tab.Offset(6))
+        o = flatbuffers.number_types.UOffsetTFlags.py_type(self._tab.Offset(8))
         if o != 0:
             return self._tab.VectorLen(o)
         return 0
 
     # Event44Message
     def ReferenceTimeIsNone(self):
-        o = flatbuffers.number_types.UOffsetTFlags.py_type(self._tab.Offset(6))
+        o = flatbuffers.number_types.UOffsetTFlags.py_type(self._tab.Offset(8))
         return o == 0
 
     # Event44Message
     def ReferenceTimeIndex(self, j):
-        o = flatbuffers.number_types.UOffsetTFlags.py_type(self._tab.Offset(8))
+        o = flatbuffers.number_types.UOffsetTFlags.py_type(self._tab.Offset(10))
         if o != 0:
             a = self._tab.Vector(o)
             return self._tab.Get(flatbuffers.number_types.Int32Flags, a + flatbuffers.number_types.UOffsetTFlags.py_type(j * 4))
@@ -69,26 +79,26 @@ class Event44Message(object):
 
     # Event44Message
     def ReferenceTimeIndexAsNumpy(self):
-        o = flatbuffers.number_types.UOffsetTFlags.py_type(self._tab.Offset(8))
+        o = flatbuffers.number_types.UOffsetTFlags.py_type(self._tab.Offset(10))
         if o != 0:
             return self._tab.GetVectorAsNumpy(flatbuffers.number_types.Int32Flags, o)
         return 0
 
     # Event44Message
     def ReferenceTimeIndexLength(self):
-        o = flatbuffers.number_types.UOffsetTFlags.py_type(self._tab.Offset(8))
+        o = flatbuffers.number_types.UOffsetTFlags.py_type(self._tab.Offset(10))
         if o != 0:
             return self._tab.VectorLen(o)
         return 0
 
     # Event44Message
     def ReferenceTimeIndexIsNone(self):
-        o = flatbuffers.number_types.UOffsetTFlags.py_type(self._tab.Offset(8))
+        o = flatbuffers.number_types.UOffsetTFlags.py_type(self._tab.Offset(10))
         return o == 0
 
     # Event44Message
     def TimeOfFlight(self, j):
-        o = flatbuffers.number_types.UOffsetTFlags.py_type(self._tab.Offset(10))
+        o = flatbuffers.number_types.UOffsetTFlags.py_type(self._tab.Offset(12))
         if o != 0:
             a = self._tab.Vector(o)
             return self._tab.Get(flatbuffers.number_types.Int32Flags, a + flatbuffers.number_types.UOffsetTFlags.py_type(j * 4))
@@ -96,26 +106,26 @@ class Event44Message(object):
 
     # Event44Message
     def TimeOfFlightAsNumpy(self):
-        o = flatbuffers.number_types.UOffsetTFlags.py_type(self._tab.Offset(10))
+        o = flatbuffers.number_types.UOffsetTFlags.py_type(self._tab.Offset(12))
         if o != 0:
             return self._tab.GetVectorAsNumpy(flatbuffers.number_types.Int32Flags, o)
         return 0
 
     # Event44Message
     def TimeOfFlightLength(self):
-        o = flatbuffers.number_types.UOffsetTFlags.py_type(self._tab.Offset(10))
+        o = flatbuffers.number_types.UOffsetTFlags.py_type(self._tab.Offset(12))
         if o != 0:
             return self._tab.VectorLen(o)
         return 0
 
     # Event44Message
     def TimeOfFlightIsNone(self):
-        o = flatbuffers.number_types.UOffsetTFlags.py_type(self._tab.Offset(10))
+        o = flatbuffers.number_types.UOffsetTFlags.py_type(self._tab.Offset(12))
         return o == 0
 
     # Event44Message
     def PixelId(self, j):
-        o = flatbuffers.number_types.UOffsetTFlags.py_type(self._tab.Offset(12))
+        o = flatbuffers.number_types.UOffsetTFlags.py_type(self._tab.Offset(14))
         if o != 0:
             a = self._tab.Vector(o)
             return self._tab.Get(flatbuffers.number_types.Int32Flags, a + flatbuffers.number_types.UOffsetTFlags.py_type(j * 4))
@@ -123,107 +133,56 @@ class Event44Message(object):
 
     # Event44Message
     def PixelIdAsNumpy(self):
-        o = flatbuffers.number_types.UOffsetTFlags.py_type(self._tab.Offset(12))
+        o = flatbuffers.number_types.UOffsetTFlags.py_type(self._tab.Offset(14))
         if o != 0:
             return self._tab.GetVectorAsNumpy(flatbuffers.number_types.Int32Flags, o)
         return 0
 
     # Event44Message
     def PixelIdLength(self):
-        o = flatbuffers.number_types.UOffsetTFlags.py_type(self._tab.Offset(12))
+        o = flatbuffers.number_types.UOffsetTFlags.py_type(self._tab.Offset(14))
         if o != 0:
             return self._tab.VectorLen(o)
         return 0
 
     # Event44Message
     def PixelIdIsNone(self):
-        o = flatbuffers.number_types.UOffsetTFlags.py_type(self._tab.Offset(12))
+        o = flatbuffers.number_types.UOffsetTFlags.py_type(self._tab.Offset(14))
         return o == 0
 
-
-def Event44MessageStart(builder):
-    builder.StartObject(5)
-
-
+def Event44MessageStart(builder): builder.StartObject(6)
 def Start(builder):
     return Event44MessageStart(builder)
-
-
-def Event44MessageAddSourceName(builder, sourceName):
-    builder.PrependUOffsetTRelativeSlot(0, flatbuffers.number_types.UOffsetTFlags.py_type(sourceName), 0)
-
-
+def Event44MessageAddSourceName(builder, sourceName): builder.PrependUOffsetTRelativeSlot(0, flatbuffers.number_types.UOffsetTFlags.py_type(sourceName), 0)
 def AddSourceName(builder, sourceName):
     return Event44MessageAddSourceName(builder, sourceName)
-
-
-def Event44MessageAddReferenceTime(builder, referenceTime):
-    builder.PrependUOffsetTRelativeSlot(1, flatbuffers.number_types.UOffsetTFlags.py_type(referenceTime), 0)
-
-
+def Event44MessageAddMessageId(builder, messageId): builder.PrependInt64Slot(1, messageId, 0)
+def AddMessageId(builder, messageId):
+    return Event44MessageAddMessageId(builder, messageId)
+def Event44MessageAddReferenceTime(builder, referenceTime): builder.PrependUOffsetTRelativeSlot(2, flatbuffers.number_types.UOffsetTFlags.py_type(referenceTime), 0)
 def AddReferenceTime(builder, referenceTime):
     return Event44MessageAddReferenceTime(builder, referenceTime)
-
-
-def Event44MessageStartReferenceTimeVector(builder, numElems):
-    return builder.StartVector(8, numElems, 8)
-
-
+def Event44MessageStartReferenceTimeVector(builder, numElems): return builder.StartVector(8, numElems, 8)
 def StartReferenceTimeVector(builder, numElems):
     return Event44MessageStartReferenceTimeVector(builder, numElems)
-
-
-def Event44MessageAddReferenceTimeIndex(builder, referenceTimeIndex):
-    builder.PrependUOffsetTRelativeSlot(2, flatbuffers.number_types.UOffsetTFlags.py_type(referenceTimeIndex), 0)
-
-
+def Event44MessageAddReferenceTimeIndex(builder, referenceTimeIndex): builder.PrependUOffsetTRelativeSlot(3, flatbuffers.number_types.UOffsetTFlags.py_type(referenceTimeIndex), 0)
 def AddReferenceTimeIndex(builder, referenceTimeIndex):
     return Event44MessageAddReferenceTimeIndex(builder, referenceTimeIndex)
-
-
-def Event44MessageStartReferenceTimeIndexVector(builder, numElems):
-    return builder.StartVector(4, numElems, 4)
-
-
+def Event44MessageStartReferenceTimeIndexVector(builder, numElems): return builder.StartVector(4, numElems, 4)
 def StartReferenceTimeIndexVector(builder, numElems):
     return Event44MessageStartReferenceTimeIndexVector(builder, numElems)
-
-
-def Event44MessageAddTimeOfFlight(builder, timeOfFlight):
-    builder.PrependUOffsetTRelativeSlot(3, flatbuffers.number_types.UOffsetTFlags.py_type(timeOfFlight), 0)
-
-
+def Event44MessageAddTimeOfFlight(builder, timeOfFlight): builder.PrependUOffsetTRelativeSlot(4, flatbuffers.number_types.UOffsetTFlags.py_type(timeOfFlight), 0)
 def AddTimeOfFlight(builder, timeOfFlight):
     return Event44MessageAddTimeOfFlight(builder, timeOfFlight)
-
-
-def Event44MessageStartTimeOfFlightVector(builder, numElems):
-    return builder.StartVector(4, numElems, 4)
-
-
+def Event44MessageStartTimeOfFlightVector(builder, numElems): return builder.StartVector(4, numElems, 4)
 def StartTimeOfFlightVector(builder, numElems):
     return Event44MessageStartTimeOfFlightVector(builder, numElems)
-
-
-def Event44MessageAddPixelId(builder, pixelId):
-    builder.PrependUOffsetTRelativeSlot(4, flatbuffers.number_types.UOffsetTFlags.py_type(pixelId), 0)
-
-
+def Event44MessageAddPixelId(builder, pixelId): builder.PrependUOffsetTRelativeSlot(5, flatbuffers.number_types.UOffsetTFlags.py_type(pixelId), 0)
 def AddPixelId(builder, pixelId):
     return Event44MessageAddPixelId(builder, pixelId)
-
-
-def Event44MessageStartPixelIdVector(builder, numElems):
-    return builder.StartVector(4, numElems, 4)
-
-
+def Event44MessageStartPixelIdVector(builder, numElems): return builder.StartVector(4, numElems, 4)
 def StartPixelIdVector(builder, numElems):
     return Event44MessageStartPixelIdVector(builder, numElems)
-
-
-def Event44MessageEnd(builder):
-    return builder.EndObject()
-
-
+def Event44MessageEnd(builder): return builder.EndObject()
 def End(builder):
     return Event44MessageEnd(builder)
--- tests/test_ev44.py
+++ tests/test_ev44.py
@@ -1,7 +1,9 @@
 import numpy as np
+import pytest
 
 from streaming_data_types import DESERIALISERS, SERIALISERS
 from streaming_data_types.eventdata_ev44 import deserialise_ev44, serialise_ev44
+from streaming_data_types.exceptions import WrongSchemaException
 
 
 class TestSerialisationEv44:
@@ -11,6 +13,7 @@ class TestSerialisationEv44:
         """
         original_entry = {
             "source_name": "some_source",
+            "message_id": 123456,
             "reference_time": [1618573589123781958, 1618573590133830371, 1618573593677164112,
                                1618573594185190549, 1618573596217316066, 1618573596725363109,
                                1618573601295720976, 1618573601799761445, 1618573607354064836],
@@ -23,6 +26,7 @@ class TestSerialisationEv44:
         entry = deserialise_ev44(buf)
 
         assert entry.source_name == original_entry["source_name"]
+        assert entry.message_id == original_entry["message_id"]
         assert np.array_equal(entry.reference_time, original_entry["reference_time"])
         assert np.array_equal(entry.reference_time_index, original_entry["reference_time_index"])
         assert np.array_equal(entry.time_of_flight, original_entry["time_of_flight"])
@@ -34,6 +38,7 @@ class TestSerialisationEv44:
         """
         original_entry = {
             "source_name": "some_source",
+            "message_id": 123456,
             "reference_time": np.array([1618573589123781958, 1618573590133830371,
                                         1618573593677164112, 1618573594185190549,
                                         1618573596217316066, 1618573596725363109,
@@ -48,10 +53,33 @@ class TestSerialisationEv44:
         entry = deserialise_ev44(buf)
 
         assert entry.source_name == original_entry["source_name"]
+        assert entry.message_id == original_entry["message_id"]
         assert np.array_equal(entry.reference_time, original_entry["reference_time"])
         assert np.array_equal(entry.reference_time_index, original_entry["reference_time_index"])
         assert np.array_equal(entry.time_of_flight, original_entry["time_of_flight"])
         assert np.array_equal(entry.pixel_id, original_entry["pixel_id"])
+
+    def test_if_buffer_has_wrong_id_then_throws(self):
+        original_entry = {
+            "source_name": "some_source",
+            "message_id": 123456,
+            "reference_time": np.array([1618573589123781958, 1618573590133830371,
+                                        1618573593677164112, 1618573594185190549,
+                                        1618573596217316066, 1618573596725363109,
+                                        1618573601295720976, 1618573601799761445,
+                                        1618573607354064836]),
+            "reference_time_index": np.array([2, 4, 5, 7]),
+            "time_of_flight": np.array([100, 200, 300, 400, 500, 600, 700, 800, 900]),
+            "pixel_id": np.array([10, 20, 30, 40, 50, 60, 70, 80, 90]),
+        }
+        buf = serialise_ev44(**original_entry)
+
+        # Manually introduce error in id.
+        buf = bytearray(buf)
+        buf[4:8] = b"1234"
+
+        with pytest.raises(WrongSchemaException):
+            deserialise_ev44(buf)
 
     def test_schema_type_is_in_global_serialisers_list(self):
         assert "ev44" in SERIALISERS
